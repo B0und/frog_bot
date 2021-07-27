73a08570cdf2d0353c7af8f2479f45b5bad08f7a
Unbound <f1arenova1@gmail.com>
2021-07-27 14:13:48 +0300

    upd cron command

--- start_frog_bot.sh
+++ start_frog_bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# @reboot ~/crackhead-bot/start_crackhead_bot.sh >> ~/crackhead-bot/logs.txt 2>&1
+# @reboot ~/frog_bot/start_frog_bot.sh >> ~/frog_bot/logs.txt 2>&1
 
 cd ~/frog_bot/
 source ~/frog_bot/venv/bin/activate
